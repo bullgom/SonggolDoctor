b02a97ca144dd3b1774997b1468f65afc8086b12
seok00 <seok0097@gmail.com>
2018-11-28 02:18:46 +0900

    병원 전화번호 다이얼 열기 추가

--- app/src/main/java/studio/sw/mobile/songgoldoctor/DiagnosisActivity.kt
+++ app/src/main/java/studio/sw/mobile/songgoldoctor/DiagnosisActivity.kt
@@ -1,7 +1,10 @@
 package studio.sw.mobile.songgoldoctor
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import java.util.*
@@ -13,6 +16,8 @@ class DiagnosisActivity : AppCompatActivity() {
     private lateinit var hospital: TextView
     private lateinit var howMany: TextView
     private lateinit var backButton: ImageButton
+    private lateinit var dialButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diagnosis)
@@ -23,10 +28,16 @@ class DiagnosisActivity : AppCompatActivity() {
         hospital = findViewById(R.id.diag_act_hospital)
         howMany = findViewById(R.id.diag_act_howMany)
         backButton = findViewById(R.id.diag_act_backButton)
+        dialButton = findViewById(R.id.action_dial)
 
 
         backButton.setOnClickListener {
             finish()
+        }
+
+        dialButton.setOnClickListener {
+            val diaIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:01023994912"))
+            startActivity(diaIntent)
         }
 
         if (intent.hasExtra("diagnosis")) {
